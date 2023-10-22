e530f940053425ae484c69ec06839890f3dde26a
Nandi <nandicurup@gmail.com>
2023-10-22 14:40:55 +0700

    [Bab 3] State Dan Props

---
+++ data.js
@@ -0,0 +1,137 @@
+export const CategoryList = [
+    {
+    id: 1,
+    categoryName: 'Popular',
+    },
+    {
+    id: 2,
+    categoryName: 'Latest',
+    },
+    {
+    id: 3,
+    categoryName: 'Product',
+    },
+    {
+    id: 4,
+    categoryName: 'Food',
+    },
+    {
+    id: 5,
+    categoryName: 'Health',
+    },
+    {
+    id: 6,
+    categoryName: 'Lifestyle',
+    },
+];
+export const BlogList = [
+    {
+    id: 1,
+    title: 'sepatu Lari terbaru',
+    category: 'sepatu',
+    image:
+'https://media.istockphoto.com/id/1350560575/id/foto/sepasang-sepatu-lari-biru-dengan-latar-belakang-putih-terisolasi.jpg?s=612x612&w=0&k=20&c=SLCTqAZS-dzguW9QdAa25KgmhibS5c3az07k9VpPTwU=',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 2,
+    title: 'Satu Set Pakaian Lari Pria',
+    category: 'Pakaian',
+    image:
+'https://media.istockphoto.com/id/186246798/photo/running-equipment-man-on-grey-background.webp?b=1&s=170667a&w=0&k=20&c=CNJn8MRx13hk2-4wfQYCqT4DIJUqnJfeP9TNGotloog=',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+        id: 3,
+        title: 'Satu Set Pakaian Lari Wanita',
+    category: 'Pakaian',
+    image:
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7IZb2-kYLQSDkinnKDp7gPMNUfP0p_HPVow&usqp=CAU',
+    createdAt: 'Oct 15, 2023',
+totalComments: 90,
+    },
+    {
+    id: 4,
+    title: 'Sepatu Nike Keluaran Terbaru',
+    category: 'Sepatu',
+    image:
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc3g3x9EHa3gI6u4o46ezM9IWSMK0D9QmyHw&usqp=CAU',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 5,
+    title: 'Kacamata Pelari',
+    category: 'Kacamata',
+    image:
+'https://media.istockphoto.com/id/1297835614/id/foto/pelari-muda-menyesuaikan-earphone-sebelum-berlari-di-pedesaan.jpg?s=612x612&w=0&k=20&c=-r9od0VTCDHy7I-Q2QYii4dYFnmI8o0_3ozBNfbcp8k=',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 6,
+    title: 'Pakaian Lari Terbaru',
+    category: 'Product',
+    image:
+'https://media.istockphoto.com/id/1064191168/id/foto/melihat-dari-atas-komposisi-dengan-peralatan-gym-terisolasi-di-atas-putih.jpg?s=612x612&w=0&k=20&c=jzCXOKNj5TjqIbEVUy7Sllx2iaX3hQ4zDe4YWMF3wn4=',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 7,
+    title: 'Sepatu Keluaran Terbaru',
+    category: 'Product',
+    image:
+'https://media.istockphoto.com/id/956501428/photo/sport-shoes-on-isolated-white-background.webp?b=1&s=170667a&w=0&k=20&c=zQ0Dsy90DcfSH5YR3yZFR26FgkBx2z_LxIhx3M-JxKg=',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 8,
+    title: 'Makanan sehat untuk pelari',
+    category: 'Food',
+    image:
+'https://media.istockphoto.com/id/590278008/photo/young-girl-eating-a-oatmeal-with-berries.webp?b=1&s=170667a&w=0&k=20&c=Eb-08qU-5PgO307OPASUwpg6S5LK8B5OXlMpOg6uKRI=',
+
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 9,
+    title: 'Makanan Sehat Untuk Pelari',
+    category: 'Food',
+    image:
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ6JPGmGy4lRb-uz8o1XPizWyyGObWqim9rA&usqp=CAU',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 10,
+    title: 'Tidur Yang cukup untuk pelari',
+    category: 'Health',
+image:
+'https://media.istockphoto.com/id/1269951365/photo/happy-man-listening-to-phone-music-with-headphones-relaxing-sleeping-or-meditating-on-green.webp?b=1&s=170667a&w=0&k=20&c=41mAWTgvdhZQDwUjfX9anw-d7vqEIfpVScPN0rO2Jcw=',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 11,
+    title: 'Minum Air Putih Yang cukup untuk pelari',
+    category: 'Health',
+    image:
+'https://media.istockphoto.com/id/1372307016/photo/shot-of-a-young-woman-taking-a-break-from-working-out-to-drink-water.webp?b=1&s=170667a&w=0&k=20&c=NwiwYVQflwsSr8fAS7PRRi1den8h8FjInBzwT_FvbMY=',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+    {
+    id: 12,
+    title: 'Peralatan Pelari',
+    category: 'Product',
+    image:
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcMFU0OxM4k9WYyG_SHXsNXgFN-p6vOL5Jrg&usqp=CAU',
+    createdAt: 'Oct 15, 2023',
+    totalComments: 90,
+    },
+];
